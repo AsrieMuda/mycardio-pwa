68977343a06f5ff9312596bad199734fc1dbef9b
AsrieMuda <asrie@moh.gov.my>
2026-09-23 21:14:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iheart-shell-v3';const ASSETS=['./','./index.html','./style.css','./app.js','./config.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='iheart-shell-v4';const ASSETS=['./','./index.html','./style.css','./app.js','./config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request).then(r=>{if(r.ok&&ASSETS.some(a=>new URL(a,self.registration.scope).href===e.request.url)){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request)))});
